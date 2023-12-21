83ae642bab2be47dd1fee39c92c102376486e18a
abedin wahid <abedinwahid9@gmail.com>
2023-12-22 00:14:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./Pages/Login/Login";
+import NavbarCompoents from "./components/Navbar/NavbarCompoents";
+
+// #DAD4D4 for text
+
+function App() {
+  return (
+    <div>
+      <NavbarCompoents></NavbarCompoents>
+      <h2>hello world</h2>
+    </div>
+  );
+}
+
+export default App;
